93af7ad07eb4431aa2adb19c9edfb5e160886ed6
Meksla <maxsigrist@utexas.edu>
2014-10-14 18:32:07 -0500

    Added Google GeoLocator function but still need to test it

--- Google-Geocode/app/src/main/java/edu/utexas/ee461l/msaj/googlegeocode/MyActivity.java
+++ Google-Geocode/app/src/main/java/edu/utexas/ee461l/msaj/googlegeocode/MyActivity.java
@@ -4,12 +4,52 @@ package edu.utexas.ee461l.msaj.googlegeocode;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.location.Geocoder;
+import android.location.Address;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MyActivity extends FragmentActivity {
+
+    public void geoLocator(View v) throws IOException{
+        Geocoder gc = new Geocoder(this);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String location = editText.getText().toString();
+
+        List<Address> list = gc.getFromLocationName(location, 1);
+
+
+        Address address = list.get(0);
+
+        double lat = address.getLatitude();
+        double lng = address.getLongitude();
+
+        String toastText = String.valueOf(lat) + String.valueOf(lng);
+
+        Toast.makeText(this, toastText , Toast.LENGTH_LONG);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        final Button button = (Button) findViewById(R.id.goButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                try {
+                    geoLocator(v);
+                }
+                catch(IOException e){
+
+                }
+            }
+        });
     }
 }
